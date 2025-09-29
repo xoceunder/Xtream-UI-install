f2fb3a706672f79d08fbc0fc0344207188ad4836
xoceunder <xoceunder@gmail.com>
2025-09-29 16:02:22 -0400

    Create updateyoutube.py

---
+++ updateyoutube.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# update panel
+import subprocess, os, sys
+from itertools import cycle, izip
+
+def updateyoutube():
+    os.system("sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl")
+    os.system("sudo chmod a+rx /usr/local/bin/youtube-dl")
+    return True
+
+if __name__ == "__main__":
+    updateyoutube()
+    #start()
